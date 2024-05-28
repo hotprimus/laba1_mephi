63f307cc7498df5af3387d5d4eb5234de310c88b
hotprimus <147264878+hotprimus@users.noreply.github.com>
2024-05-29 00:36:14 +0300

    Create testmatrix.h

---
+++ testmatrix.h
@@ -0,0 +1,14 @@
+#ifndef TEST_MATRIX_H
+#define TEST_MATRIX_H
+
+#include "matrix.h"
+
+void test_InputMatrix();
+void test_OutputMatrix();
+void test_InitMatrix();
+void test_CleanMemory();
+void test_SumOfMatrix();
+void test_MultiplicationOfMatrix();
+void test_AddLinearCombination();
+
+#endif // TEST_MATRIX_H
